66c30d366645329e306d7c354624a569d13e1bab
GanapathySubramaniam <105225199+GanapathySubramaniam@users.noreply.github.com>
2024-07-01 14:01:00 -0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -7,7 +7,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 if "authentication_status" not in st.session_state:
-    st.session_state["authentication_status"]=False
+    st.session_state["authentication_status"]=''
 st.title('Login 🗝️')
 
 login()
