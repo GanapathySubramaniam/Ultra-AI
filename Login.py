90ff38076d2ef175c99fcda6ce44224db2a60466
GanapathySubramaniam <105225199+GanapathySubramaniam@users.noreply.github.com>
2024-07-01 20:18:16 +0000

    login

--- Login.py
+++ Login.py
@@ -1,13 +1,12 @@
 import streamlit as st
 from auth import login
-st.session_state["authentication_status"]=False
+
 st.set_page_config(
     page_title="Ultra AI",
     page_icon="🧊",
     initial_sidebar_state="expanded",
 )
-if "authentication_status" not in st.session_state:
-    st.session_state["authentication_status"]=''
+
 st.title('Login 🗝️')
 
 login()
