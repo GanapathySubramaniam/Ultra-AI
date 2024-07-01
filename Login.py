9af5ea5a8a7babc2cc2c4cdc2d42c9eb7143f60f
GanapathySubramaniam <105225199+GanapathySubramaniam@users.noreply.github.com>
2024-07-01 03:42:17 +0000

    commmit

--- Login.py
+++ Login.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from auth import login
+
+st.set_page_config(
+    page_title="Ultra AI",
+    page_icon="🧊",
+    initial_sidebar_state="expanded",
+)
 if "authentication_status" not in st.session_state:
     st.session_state["authentication_status"]=False
 st.title('Login 🗝️')
